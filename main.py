1ec3cb37879f894667209b3f65cbd0e118b85ac3
Alex <alexerombou@gmail.com>
2024-12-02 23:17:57 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
