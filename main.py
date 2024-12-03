819221af18e1f211ba011617f5152282d5b1e7c9
Alex <alexerombou@gmail.com>
2024-12-03 11:11:10 -0600

    Added footer, header, navbar, and css.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
